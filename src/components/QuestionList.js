dfa609c9a8c026d89dd537ee417bf0639c21d2a8
tjayearl <iamtjayearl@gmail.com>
2025-04-24 10:18:55 +0300

    Fix fetch issue and add question form handling

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import QuestionItem from "./QuestionItem";
 
-function QuestionList({ questions, onDelete, onUpdate }) {
+function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Fetch error:", error));
+  }, []);
+
+  function handleDelete(id) {
+    const updatedQuestions = questions.filter((q) => q.id !== id);
+    setQuestions(updatedQuestions);
+  }
+
+  function handleUpdate(updatedQuestion) {
+    const updatedQuestions = questions.map((q) =>
+      q.id === updatedQuestion.id ? updatedQuestion : q
+    );
+    setQuestions(updatedQuestions);
+  }
+
+  const questionItems = questions.map((question) => (
+    <QuestionItem
+      key={question.id}
+      question={question}
+      onDeleteQuestion={handleDelete}
+      onUpdateQuestion={handleUpdate}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>
-        {questions.map((question) => (
-          <QuestionItem
-            key={question.id}
-            question={question}
-            onDelete={onDelete}
-            onUpdate={onUpdate}
-          />
-        ))}
-      </ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
