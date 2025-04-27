490fa7c7c472e8421a4d3e5d5e2857b3a3be3524
tjayearl <iamtjayearl@gmail.com>
2025-04-27 22:32:10 +0300

    Update QuestionList.js

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,45 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  const [questions, setQuestions] = useState([]);
-
-  // Fetch questions when the component mounts
-  useEffect(() => {
-    fetch("http://localhost:4000/questions")
-      .then((res) => res.json())
-      .then((data) => setQuestions(data))
-      .catch((error) => console.error("Fetch error:", error));
-  }, []);
-
-  // Delete a question
-  function handleDelete(id) {
-    const updatedQuestions = questions.filter((q) => q.id !== id);
-    setQuestions(updatedQuestions);
-  }
-
-  // Update a question (e.g., when dropdown value changes)
-  function handleUpdate(updatedQuestion) {
-    const updatedQuestions = questions.map((q) =>
-      q.id === updatedQuestion.id ? updatedQuestion : q
-    );
-    setQuestions(updatedQuestions);
-  }
-
-  // Map through questions to create QuestionItem components
-  const questionItems = questions.map((question) => (
-    <QuestionItem
-      key={question.id}
-      question={question}
-      onDeleteQuestion={handleDelete}
-      onUpdateQuestion={handleUpdate}
-    />
-  ));
-
+function QuestionList({ questions, onDelete, onUpdate }) {
   return (
     <section>
-      <h1>Quiz Questions</h1>
-      <ul>{questionItems}</ul>
+      <h1>Questions List</h1>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDelete={onDelete}
+            onUpdate={onUpdate} 
+          />
+        ))}
+      </ul>
     </section>
   );
 }
